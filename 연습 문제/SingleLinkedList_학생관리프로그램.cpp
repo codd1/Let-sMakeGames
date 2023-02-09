ce8f9c2f3234ac8b0568b384a2e2b41d933a62fd
hong <mgs08041@naver.com>
2023-02-10 04:24:20 +0900

    학생 관리 프로그램 - 학생 탐색 및 삭제 구현 완료

--- 연습 문제/SingleLinkedList_학생관리프로그램.cpp
+++ 연습 문제/SingleLinkedList_학생관리프로그램.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 
 using namespace std;
+/*
+숙제: 학생 관리 프로그램을 더블링크드리스트를 활용해 만들어보자.
+*/
 
 enum MAIN_MENU {
 	MM_NONE,
@@ -80,7 +83,7 @@ int OutputMenu() {
 	system("cls");
 	cout << "1. 학생추가" << endl;
 	cout << "2. 학생삭제" << endl;
-	cout << "3. 학생탐색" << endl;
+	cout << "3. 학생검색" << endl;
 	cout << "4. 학생출력" << endl;
 	cout << "5. 종료" << endl;
 	cout << "메뉴를 선택하세요: ";
@@ -131,19 +134,6 @@ void Insert(PLIST pList) {
 	pList->pEnd = pNode;
 }
 
-void ClearList(PLIST pList) {
-	PNODE pNode = pList->pBegin;
-
-	while (pNode != NULL) {
-		PNODE pNext = pNode->pNext;
-		delete pNode;
-		pNode = pNext;
-	}
-	pList->pBegin = NULL;
-	pList->pEnd = NULL;
-	pList->iSize = 0;
-}
-
 // const 포인터이기 때문에 가리키는 대상의 값을 변경할 수 없다.
 void OutputStudent(const PSTUDENT pStudnet) {
 	cout << "이름: " << pStudnet->strName << "\t학번: " << pStudnet->iNumber << endl;
@@ -166,6 +156,87 @@ void Output(PLIST pList) {
 	system("pause");
 }
 
+void ClearList(PLIST pList) {
+	PNODE pNode = pList->pBegin;
+
+	while (pNode != NULL) {
+		PNODE pNext = pNode->pNext;
+		delete pNode;
+		pNode = pNext;
+	}
+	pList->pBegin = NULL;
+	pList->pEnd = NULL;
+	pList->iSize = 0;
+}
+
+void Search(PLIST pList) {
+	system("cls");
+	cout << "=================== 학생검색 ===================" << endl;
+
+	cout << "검색할 이름을 입력하세요: ";
+	char strName[NAME_SIZE] = {};
+	InputString(strName, NAME_SIZE);
+
+	PNODE pNode = pList->pBegin;
+
+	while (pNode != NULL) {
+		if (strcmp(strName, pNode->tStudent.strName) == 0) {
+			OutputStudent(&pNode->tStudent);
+			system("pause");
+			return;
+		}
+		pNode = pNode->pNext;
+	}
+
+	cout << "찾을 학생이 없습니다." << endl;
+	system("pause");
+}
+
+void Delete(PLIST pList) {
+	system("cls");
+	cout << "=================== 학생삭제 ===================" << endl;
+
+	cout << "삭제할 이름을 입력하세요: ";
+	char strName[NAME_SIZE] = {};
+	InputString(strName, NAME_SIZE);
+
+	PNODE pNode = pList->pBegin;
+	PNODE pPrev = NULL;
+
+	while (pNode != NULL) {
+		if (strcmp(strName, pNode->tStudent.strName) == 0) {
+			// 지울 노드의 다음 노드를 얻어온다.
+			PNODE pNext = pNode->pNext;
+
+			if (pPrev == NULL) {	// 첫 번째 노드를 지운다는 의미
+				delete pNode;
+				pList->pBegin = pNext;
+
+				if (pNext == NULL) {	// 노드가 하나인 경우
+					pList->pEnd = NULL;
+				}
+			}
+			else {		// 이전 노드가 있을 경우, 이전 노드의 다음을 지운 노드의 다음 노드로 연결해준다.
+				delete pNode;
+				pPrev->pNext = pNext;
+
+				if (pNext == NULL) {	// 마지막 노드를 지우는 경우
+					pList->pEnd = pPrev;
+				}
+			}
+			cout << strName << " 학생 삭제 완료" << endl;
+			pList->iSize--;
+
+			system("pause");
+			return;
+		}
+		pPrev = pNode;
+		pNode = pNode->pNext;
+	}
+	cout << "삭제할 학생을 찾을 수 없습니다." << endl;
+	system("pause");
+}
+
 int main() {
 
 	LIST tList;
@@ -185,8 +256,10 @@ int main() {
 			tList.iSize++;
 			break;
 		case MM_DELETE:
+			Delete(&tList);
 			break;
 		case MM_SEARCH:
+			Search(&tList);
 			break;
 		case MM_OUTPUT:
 			Output(&tList);
