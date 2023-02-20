3a12cc05cb1d769cb999c5db3b0978be254456dc
hong <mgs08041@naver.com>
2023-02-20 21:29:07 +0900

    생성자와 소멸자, 오버로딩

--- 기본 문법/class.cpp
+++ 기본 문법/class.cpp
@@ -18,9 +18,46 @@ using namespace std;
 	상속성: 클래스는 클래스와 클래스 간에 부모, 자식 관계를 형성할 수 있다.
 			자식 클래스는 부모클래스의 멤버를 물려받아 자신의 것처럼 사용할 수 있다. 단, private로 되어 있는 멤버에는 접근이 불가능하다.
 	다형성: 부모 자식관계로 상속관계가 형성되어 있는 클래스 간에 서로 형변환이 가능한 성질을 말한다.
+
+	생성자와 소멸자
+	생성자: 어떤 클래스를 이용해서 객체를 생성할 때 자동으로 호출되는 함수이다. 객체 생성 시 호출되는 함수이기 때문에 객체의 멤버변수를 초기화할 때 유용하게 사용할 수 있다.
+	생성자를 만들어주지 않을 경우 내부적으로 기본 생성자가 제공 되어 기본 생성자를 자동으로 호출해주게 된다.
+	클래스명(){
+
+	} 의 형태
+
+	소멸자: 어떤 클래스를 이용해서 생성한 객체가 메모리에서 해제될 때(여기서는 메인 함수가 종료될 때) 자동으로 호출되는 함수이다. 객체를 사용하고 난 뒤 마무리 작업을 해줄 때 유용하게 사용할 수 있다.
+	~클래스명(){
+
+	} 의 형태
 */
 
 class CPlayer {
+
+public:		// private이면 메인함수에서 접근이 불가능해 오류 발생!
+	CPlayer() {			// 생성자
+		cout << "Player 생성자" << endl;
+		strcpy_s(m_strName, "플레이어");
+	}
+
+	CPlayer(const char* pName) {		// 오버로딩
+		strcpy_s(m_strName, pName);
+		cout << "Name 생성자" << endl;
+	}
+
+	CPlayer(const char* pName, int iFlash, int iBack) :		// 오버로딩
+		m_iBack(iBack),
+		m_iFlash(iFlash)
+	{
+		strcpy_s(m_strName, pName);
+		//m_iFlash = iFlash;
+
+		cout << "Name, Flash, Back 생성자" << endl;
+	}
+
+	~CPlayer() {		// 소멸자
+		cout << "Player 소멸자" << endl;
+	}
 	// 클래스 멤버변수를 선언할 때는 m_을 붙여준다. (개인 스타일)
 	char m_strName[32];
 
@@ -30,16 +67,18 @@ public:		// public 키워드 아래에 있는 멤버는 모두 외부에서 접근이 가능하다.
 
 private:
 	int m_iFlash;
+	int m_iBack;
 
 public:
 	void Output() {
 		cout << "Player Output" << endl;
+		cout << "Name: " << m_strName << endl;
 	}
 
 };
 
 int main() {
-	CPlayer ply1;
+	CPlayer ply1("coddl", 3, 1);
 
 	// main() 함수 안은 클래스 외부이기 때문에 public으로 설정되어있는 멤버에만 접근이 가능하다.
 	// ply1.m_iFlash;	// Error!
